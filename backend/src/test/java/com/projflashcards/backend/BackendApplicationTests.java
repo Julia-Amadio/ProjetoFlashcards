f05052b33bd0efab70ed921e8238d9d69d28731e
Julia-Amadio <98353170+Julia-Amadio@users.noreply.github.com>
2026-04-14 13:00:05 -0300

    chore: first commit com a estrutura base Spring Boot e Python

---
+++ backend/src/test/java/com/projflashcards/backend/BackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.projflashcards.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
